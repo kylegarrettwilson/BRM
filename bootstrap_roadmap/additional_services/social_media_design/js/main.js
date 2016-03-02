057eb67b7f205fcfe01c1dedabad3240442f8b41
kylegarrettwilson <LetItBe@Kyles-iMac.local>
2016-03-01 20:52:53 -0800

    work

--- bootstrap_roadmap/additional_services/social_media_design/js/main.js
+++ bootstrap_roadmap/additional_services/social_media_design/js/main.js
@@ -1,3 +1,64 @@
-/**
- * Created by LetItBe on 10/12/15.
- */
+
+$( "#facebook_button" ).click(function() {
+    $( "#facebook" ).show( "bounce", 1000 );
+});
+
+$("#facebook").click(function(){
+    $( "#facebook" ).hide( "slow");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+$( "#google_button" ).click(function() {
+    $( "#google" ).show( "bounce", 1000 );
+});
+
+$("#google").click(function(){
+    $( "#google" ).hide( "slow");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+$( "#twitter_button" ).click(function() {
+    $( "#twitter" ).show( "bounce", 1000 );
+});
+
+$("#twitter").click(function(){
+    $( "#twitter" ).hide( "slow");
+});
+
+
+
+
+
+
+
+
+
+
+
+$( "#youtube_button" ).click(function() {
+    $( "#youtube" ).show( "bounce", 1000 );
+});
+
+$("#youtube").click(function(){
+    $( "#youtube" ).hide( "slow");
+});
