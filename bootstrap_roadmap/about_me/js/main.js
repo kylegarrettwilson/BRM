b306292778d443bd672bf7deb53366516bb97275
kylegarrettwilson <LetItBe@Kyles-iMac.local>
2016-08-03 21:09:37 -0700

    work

--- bootstrap_roadmap/about_me/js/main.js
+++ bootstrap_roadmap/about_me/js/main.js
@@ -1,19 +1,24 @@
-$( "#image" ).click(function() {
-    $( "#bio" ).show( "bounce", 1000 );
+
+
+$( document ).ready(function() {
+
+
+    $("#image").click(function () {
+        $("#bio").show("bounce", 1000);
+    });
+
+    $("#bio").click(function () {
+        $("#bio").hide("slow");
+    });
+
+
+    $("#image").mouseover(function () {
+        $("#image").css("background-image", "url(images/about_me2.jpg)");
+    });
+
+    $("#image").mouseout(function () {
+        $("#image").css("background-image", "url(images/about_me.jpg)");
+    });
+
+
 });
-
-$("#bio").click(function(){
-    $( "#bio" ).hide( "slow");
-});
-
-
-
-
-$("#image").mouseover(function() {
-    $("#image").css("background-image", "url(images/about_me2.jpg)");
-});
-
-$("#image").mouseout(function() {
-    $("#image").css("background-image", "url(images/about_me.jpg)");
-});
-
